c9747462b1f5e95c332e087249a68ff53d9a3886
redect3912 <redect3912@protonmail.com>
2025-06-21 21:49:15 +0200

    Asks for users favourite number and prints it back

---
+++ src/FavNr.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Enter your favourite number: ";
+
+    int favNr = 0;
+    cin >> favNr;
+
+    cout << "Your favourite number is " << favNr << "." << endl;
+
+    return 0;
+}
