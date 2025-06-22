b51e7c0f813d7275e6fa94de7556fb8a6374b5ae
redect3912 <redect3912@protonmail.com>
2025-06-22 20:25:37 +0200

    Enters date using multiple cin inputs on one line

---
+++ src/enterDate.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Enter the date with spaces: ";
+    int day,month,year;
+    cin >> day >> month >> year;
+    cout << "The date is " << day << "-" << month << "-" << year << endl;
+    return 0;
+}
