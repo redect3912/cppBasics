a6c4f2af7e2233edd40675193bdbe68e98e9380a
redect3912 <redect3912@protonmail.com>
2025-06-23 19:16:07 +0200

    calculates budget and total expenses

---
+++ src/budget.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+/****************************************************************
+ * 
+ * Program let's user calculate their income and expenses
+ * 
+ ****************************************************************/
+
+using namespace std;
+
+int main () {
+    int income = 0, rent = 0, groceries = 0, invest = 0, clothes = 0, subscriptions = 0, healthcare = 0, savings = 0, sports = 0, other = 0;
+
+    cout << "You can enter your monthly income and expenses here to get a concise overview" << endl;
+    cout << "What is your income?: ";
+    cin >> income;
+
+    cout << "What is do you spent on rent?: ";
+    cin >> rent;
+
+    cout << "What is do you spent on groceries?: ";
+    cin >> groceries;
+
+    cout << "How much do you invest?: ";
+    cin >> invest;
+
+    cout << "What is do you spent on clothes?: ";
+    cin >> clothes;
+
+    cout << "What is do you spent on subscriptions: ";
+    cin >> subscriptions;
+
+    cout << "What is do you spent on healthcare?: ";
+    cin >> healthcare;
+
+    cout << "How much do you save?: ";
+    cin >> savings;
+
+    cout << "What do you spent on sports?: ";
+    cin >> sports;
+
+    cout << "What is do you spent on other things?: ";
+    cin >> other;
+
+    int expenses = rent + groceries + invest + clothes + subscriptions + healthcare + savings + sports + other;
+    cout << "Your total income is " << income << " and your total expenses are " << expenses << endl;
+    return 0;
+}
