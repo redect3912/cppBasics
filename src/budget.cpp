7380c1de4ea5535ac094c9d0c066922332ffdf29
redect3912 <redect3912@protonmail.com>
2025-06-25 22:44:47 +0200

    Used array and shortened the in/output needed

--- src/budget.cpp
+++ src/budget.cpp
@@ -9,41 +9,20 @@
 using namespace std;
 
 int main () {
-    double income = 0, rent = 0, groceries = 0, invest = 0, clothes = 0, subscriptions = 0, healthcare = 0, savings = 0, sports = 0, other = 0;
 
-    cout << "You can enter your monthly income and expenses here to get a concise overview" << endl;
+    int income = 0;
     cout << "What is your income?: ";
     cin >> income;
 
-    cout << "What is do you spent on rent?: ";
-    cin >> rent;
+    cout << "Please fill in your expenses in the order of rent, investing, savings, groceries, healthcare, sports, clothes, subscriptions and other: ";
+    double expenses [9] = {0};
+    cin >> expenses[0] >> expenses[1] >> expenses[2] >> expenses[3] >> expenses[4] >> expenses[5] >> expenses[6] >> expenses[7] >> expenses[8];
 
-    cout << "What is do you spent on groceries?: ";
-    cin >> groceries;
+    double totalExpenses = expenses[0] + expenses[1] + expenses[2] + expenses[3] + expenses[4] + expenses[5] + expenses[6] + expenses[7] + expenses[8];
 
-    cout << "How much do you invest?: ";
-    cin >> invest;
+    double balance = income - totalExpenses;
 
-    cout << "What is do you spent on clothes?: ";
-    cin >> clothes;
+    cout << "Your total income is " << income << " and your total expenses are " << totalExpenses << " which gives a balance of " << balance << endl;
 
-    cout << "What is do you spent on subscriptions: ";
-    cin >> subscriptions;
-
-    cout << "What is do you spent on healthcare?: ";
-    cin >> healthcare;
-
-    cout << "How much do you save?: ";
-    cin >> savings;
-
-    cout << "What do you spent on sports?: ";
-    cin >> sports;
-
-    cout << "What is do you spent on other things?: ";
-    cin >> other;
-
-    double expenses = rent + groceries + invest + clothes + subscriptions + healthcare + savings + sports + other;
-    double balance = income - expenses;
-    cout << "Your total income is " << income << " and your total expenses are " << expenses << " which gives a balance of " << balance << endl;
     return 0;
 }
