ff289a310a651641df3a02f0374d257e32de5664
redect3912 <redect3912@protonmail.com>
2025-06-27 17:38:06 +0200

    Shows how vectors can change size with pushback syntax

--- src/vectors.cpp
+++ src/vectors.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
     
-    vector <int> test_scores {98,34,65,32,85};
+    vector <int> test_scores {98,34};
 
     cout << "The first score at index 0: " << test_scores[0] << endl; //array syntax
     cout << "The second score at index 1: " << test_scores.at(1) << endl; //vector syntax
@@ -17,6 +17,12 @@ int main() {
 
     cout << "The first score at index 0 is now: " << test_scores.at(0) << endl;
     cout << "The second score at index 1 is now: " << test_scores.at(1) << endl;
+
+    cout << "The vector is still size 2, but it can scale when we add another element." << endl;
+    cout << "We use \"test_scores.push_back(80)\" for that" << endl;
+    test_scores.push_back(80);
+
+    cout << "there is now a third element which is at index 2 and has value " << test_scores.at(2) << endl;
 
 
     return 0;
