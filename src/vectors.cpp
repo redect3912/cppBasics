8805e0065328cc244a244773267a4b9e0d3c0e2c
redect3912 <redect3912@protonmail.com>
2025-06-28 14:55:31 +0200

    adds example of 2D vectors

--- src/vectors.cpp
+++ src/vectors.cpp
@@ -28,23 +28,46 @@ int main() {
     //cin >> test_scores.at(3); checks for bounds, out of range gives error message
     
 
-    vector <int> testScores = {45,65,83,27};
+    // vector <int> testScores = {45,65,83,27};
 
-    cout << "Enter a test score to add to the vector: ";
+    // cout << "Enter a test score to add to the vector: ";
 
-    int scoreToAdd = 0;
-    cin >> scoreToAdd;
+    // int scoreToAdd = 0;
+    // cin >> scoreToAdd;
 
-    testScores.push_back(scoreToAdd); //adds integer scoreToAdd to end of vector
+    // testScores.push_back(scoreToAdd); //adds integer scoreToAdd to end of vector
 
-    cout << "The test scores are now: " << endl;
+    // cout << "The test scores are now: " << endl;
 
-    cout << testScores.at(0) << endl;
-    cout << testScores.at(1) << endl;
-    cout << testScores.at(2) << endl;
-    cout << testScores.at(3) << endl;
-    cout << testScores.at(4) << endl;
+    // cout << testScores.at(0) << endl;
+    // cout << testScores.at(1) << endl;
+    // cout << testScores.at(2) << endl;
+    // cout << testScores.at(3) << endl;
+    // cout << testScores.at(4) << endl;
     
-    cout << "There are now " << testScores.size() << " scores in the vector" << endl;
+    // cout << "There are now " << testScores.size() << " scores in the vector" << endl;
+
+    // 2-D vectors
+
+    vector <vector<int>> movieRatings
+    {
+        {1,2,3,4}, //rows are the revieuwers, columns are the movies
+        {1,2,4,4},
+        {1,3,4,5}
+    };
+
+    //we can again use array and vector syntax to print out values, first array:
+
+    cout <<movieRatings[0][0] << endl;
+    cout <<movieRatings[0][1] << endl;
+    cout <<movieRatings[0][2] << endl;
+    cout <<movieRatings[0][3] << endl;
+
+    cout << movieRatings.at(0).at(1) << endl;
+    cout << movieRatings.at(0).at(2) << endl;
+    cout << movieRatings.at(0).at(3) << endl;
+    cout << movieRatings.at(0).at(4) << endl;
+
+
     return 0;
 }
