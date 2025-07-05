8917fd059320fa119cd27cd2eb803f3f7bf33c78
redect3912 <redect3912@protonmail.com>
2025-07-05 21:40:42 +0200

    add yearly income expenses and balance

--- src/budget.cpp
+++ src/budget.cpp
@@ -10,19 +10,24 @@ using namespace std;
 
 int main () {
 
-    int income = 0;
-    cout << "What is your income?: ";
-    cin >> income;
+    int monthlyIncome = 0;
+    cout << "What is your monthly income?: ";
+    cin >> monthlyIncome;
 
-    cout << "Please fill in your expenses in the order of rent, investing, savings, groceries, healthcare, sports, clothes, subscriptions and other: ";
+    cout << "Please fill in your monthly expenses in the order of rent, investing, savings, groceries, healthcare, sports, clothes, subscriptions and other: ";
     double expenses [9] = {0};
     cin >> expenses[0] >> expenses[1] >> expenses[2] >> expenses[3] >> expenses[4] >> expenses[5] >> expenses[6] >> expenses[7] >> expenses[8];
 
-    double totalExpenses = expenses[0] + expenses[1] + expenses[2] + expenses[3] + expenses[4] + expenses[5] + expenses[6] + expenses[7] + expenses[8];
+    double monthlyExpenses = expenses[0] + expenses[1] + expenses[2] + expenses[3] + expenses[4] + expenses[5] + expenses[6] + expenses[7] + expenses[8];
 
-    double balance = income - totalExpenses;
+    double monthlyBalance = monthlyIncome - monthlyExpenses;
+    double yearlyBalance = monthlyBalance * 12;
 
-    cout << "Your total income is " << income << " and your total expenses are " << totalExpenses << " which gives a balance of " << balance << endl;
+    double yearlyIncome = monthlyIncome * 12;
+    double yearlyExpenses = monthlyExpenses * 12;
+
+    cout << "Your monthly income is " << monthlyIncome << " and your monthly expenses are " << monthlyExpenses << " which gives a moonthly balance of " << monthlyBalance << endl;
+    cout << "Your yearly income is " << yearlyIncome << " and your yearly expenses are " << yearlyExpenses << " which gives a yearly balance of " << yearlyBalance << endl;
 
     return 0;
 }
