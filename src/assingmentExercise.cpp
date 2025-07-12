26fd14197b62f5c26f9e321b32fefbd423635810
redect3912 <redect3912@protonmail.com>
2025-07-12 21:41:18 +0200

    Simple assingment operation

---
+++ src/assingmentExercise.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int num1 = 10;
+    int num2 = 0;
+    
+    num1 = 5;
+    
+    num2 = num1; //assigns value of num1 to num2
+   
+    cout << num1 << " " << num2;
+
+    return 0;
+}
