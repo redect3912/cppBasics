3a8a0168b5c3513ff083bbd9f1cdcf5dc3175bee
redect3912 <redect3912@protonmail.com>
2025-07-21 13:20:05 +0200

    style update

--- src/calculateChangeModulo.cpp
+++ src/calculateChangeModulo.cpp
@@ -10,21 +10,21 @@ int main() {
 	cin >> cents;
 
 	euros = cents / 100;
-    cents = cents % 100;
+	cents = cents % 100;
 
 	fiftyCents = cents / 50;
-    cents %= 50;
+    	cents %= 50;
 
 	twentyCents = cents / 20;
-    cents %= 20;
+    	cents %= 20;
 
 	tenCents = cents / 10;
-    cents %= 10;
+    	cents %= 10;
 
 	fiveCents = cents / 5;
-    cents %= 5;
+    	cents %= 5;
 
-    oneCents = cents / 1;
+    	oneCents = cents / 1;
 
 	cout << "You can provide this change as follows" << endl;
 	cout << "Euros: " << euros << endl;
@@ -32,8 +32,8 @@ int main() {
 	cout << "Twenty cents: " << twentyCents << endl;
 	cout << "Ten cents: " << tenCents << endl;
 	cout << "Five cents: " << fiveCents << endl;
-    cout << "One cents: " << oneCents << endl;
+    	cout << "One cents: " << oneCents << endl;
 
 	cout << endl;
-    return 0;
+    	return 0;
 }
