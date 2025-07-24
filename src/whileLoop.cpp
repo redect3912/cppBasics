c7df97fcd2119a3a144b7cf16aeeb7aa2f418233
redect3912 <redect3912@protonmail.com>
2025-07-24 14:48:46 +0200

    intro to while loops

---
+++ src/whileLoop.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    //print even nrs <= 10
+    // int i {1};
+
+    // while (i <= 10) {
+    //     if (i%2 == 0) {
+    //         cout << i << endl;
+            
+    //     }
+    //     ++i;
+    // }
+
+    //loop over and print array elements
+    // int scores [] {83,34,55,93};
+    // int i{0};
+
+    // while (i<4) {
+    //     cout << scores [i] << endl;
+    //     ++i;
+    // }
+
+    // countinously ask for correct input
+    
+    // int nr{};
+    // cout << "Enter an integer less than 100: ";
+    // cin >> nr;
+
+    // while (nr >= 100) {
+    //     cout << "Enter an integer less than 100: ";
+    //     cin >> nr;
+    // }
+
+    // cout << "You entered " << nr << endl;
+    
+
+    //    continously ask for int between 1 and 5 with bool done
+    //    bool done {false};
+    //    int number {0};
+    //
+    //    while (!done) {
+    //           cout << "Enter an integer between 1 and 5: ";
+    //           cin >> number;
+    //           if (number <=1 || number >=5)
+    //               cout << "Out of range, try again" << endl;
+    //           else {
+    //               cout << "Thanks!" << endl;
+    //               done = true;
+    //            }
+    //       }
+
+    
+    
+    
+    return 0;
+}
